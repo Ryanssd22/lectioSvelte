54b9a5264fe47cc4b0e3049db8f6eee4cb408b7f
Ryanssd22 <47930762+Ryanssd22@users.noreply.github.com>
2025-06-28 08:40:56 -0500

    fix maybe

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,11 +1,11 @@
-// export async function handle({ event, resolve }) {
-//   const response = await resolve(event);
-//   
-//   //Http caching
-//   if (event.url.pathname.endsWith('.wasm')) {
-//     response.headers.set('Cache-Control', 'public, max-age=31536000, immutable');
-//     response.headers.set('Content-Type', 'application/wasm');
-//   }
-//   
-//   return response;
-// }
+export async function handle({ event, resolve }) {
+  const response = await resolve(event);
+  
+  //Http caching
+  if (event.url.pathname.endsWith('.wasm')) {
+    response.headers.set('Cache-Control', 'public, max-age=31536000, immutable');
+    response.headers.set('Content-Type', 'application/wasm');
+  }
+  
+  return response;
+}
