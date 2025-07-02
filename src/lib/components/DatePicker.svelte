2124a2e0e480e21d662de81a8588a0bd745f5a6a
Ryanssd22 <carbox7@gmail.com>
2025-07-02 17:01:48 -0500

    Calendar

--- src/lib/components/DatePicker.svelte
+++ src/lib/components/DatePicker.svelte
@@ -5,11 +5,13 @@
 	import MaterialSymbolsBookRibbonOutlineRounded from '~icons/material-symbols/book-ribbon-outline-rounded';
 	import MaterialSymbolsFormatLineSpacingRounded from '~icons/material-symbols/format-line-spacing-rounded';
 	import MaterialSymbolsRefreshRounded from '~icons/material-symbols/refresh-rounded';
+	import MaterialSymbolsCalendarMonthOutlineRounded from '~icons/material-symbols/calendar-month-outline-rounded';
 
-	import { DropdownMenu } from 'bits-ui';
+	import { DropdownMenu, DatePicker } from 'bits-ui';
 
 	import { fly } from 'svelte/transition';
 	import { SvelteDate } from 'svelte/reactivity';
+	import { CalendarDate } from '@internationalized/date';
 
 	let {
 		date = $bindable(),
@@ -28,6 +30,9 @@
 		dateToSearch.setHours(0, 0, 0, 0);
 		onToday = today.getTime() == dateToSearch.getTime();
 	}
+	let calendarDate = $state(
+		new CalendarDate(date.getFullYear(), date.getMonth() + 1, date.getDate())
+	);
 
 	let title = $derived.by(() => {
 		if (liturgy) {
@@ -58,12 +63,10 @@
 	}
 	function setDateToday() {
 		let today = new Date(Date.now());
-		date.setFullYear(today.getFullYear());
-		date.setMonth(today.getMonth());
-		date.setDate(today.getDate());
-		updateLiturgy();
+		setNewDate(today);
 	}
 	function updateLiturgy() {
+		calendarDate = new CalendarDate(date.getFullYear(), date.getMonth() + 1, date.getDate());
 		formattedDate = formatDate();
 		readingIndex = 0;
 		updateOnToday();
@@ -73,6 +76,23 @@
 		readingIndex = value;
 		console.log(value);
 	}
+
+	function calendarSelection() {
+		if (calendarDate) {
+			let formattedDate = new Date(`${calendarDate}T00:00:00`);
+			console.log(`Date chosen: ${calendarDate}`);
+			console.log(`Formatted Date: ${formattedDate}`);
+			setNewDate(formattedDate);
+		} else {
+			calendarDate = new CalendarDate(date.getFullYear(), date.getMonth() + 1, date.getDate());
+		}
+	}
+	function setNewDate(formattedDate) {
+		date.setFullYear(formattedDate.getFullYear());
+		date.setMonth(formattedDate.getMonth());
+		date.setDate(formattedDate.getDate());
+		updateLiturgy();
+	}
 </script>
 
 <div class="w-full md:w-5/6">
@@ -81,7 +101,7 @@
 			<MaterialSymbolsArrowLeftAltRounded class="arrowIcon group-active:scale-90" />
 		</button>
 		<div class="mx-1 flex w-full flex-col items-center">
-			<div class="relative flex min-h-10 w-full items-end justify-end overflow-hidden py-1">
+			<div class="relative flex min-h-10 w-full items-end justify-end py-1">
 				<div
 					in:fly={{ duration: 100, delay: 100, y: titleFly }}
 					out:fly={{ duration: 100, y: -titleFly }}
@@ -161,10 +181,83 @@
 						/>
 					{/if}
 				</div>
-				<!-- <div> -->
-				<p class="justify-self-center">{formattedDate}</p>
-				<!-- </div> -->
-				<div class=""></div>
+
+				<DatePicker.Root
+					weekdayFormat="short"
+					bind:value={calendarDate}
+					onValueChange={calendarSelection}
+				>
+					<DatePicker.Input>
+						<DatePicker.Trigger>
+							<div class="group flex cursor-pointer items-center">
+								<p class="mx-1 justify-self-center">{formattedDate}</p>
+
+								<MaterialSymbolsCalendarMonthOutlineRounded
+									class="opacity-30 transition group-hover:opacity-100"
+								/>
+							</div>
+						</DatePicker.Trigger>
+					</DatePicker.Input>
+
+					<DatePicker.Content sideOffset={8} class="z-[999]">
+						<DatePicker.Calendar class="rounded-xl bg-amber-100 p-[22px] shadow-xl">
+							{#snippet children({ months, weekdays })}
+								<DatePicker.Header class="flex items-center justify-between">
+									<DatePicker.PrevButton
+										class="inline-flex size-10 items-center justify-center rounded-lg transition-all hover:bg-amber-200 active:scale-[0.98]"
+									>
+										<MaterialSymbolsArrowLeftAltRounded class="size-7" />
+									</DatePicker.PrevButton>
+									<DatePicker.Heading class="text-lg font-medium" />
+									<DatePicker.NextButton
+										class="inline-flex size-10 items-center justify-center rounded-lg transition-all hover:bg-amber-200 active:scale-[0.98]"
+									>
+										<MaterialSymbolsArrowRightAltRounded class="size-7" />
+									</DatePicker.NextButton>
+								</DatePicker.Header>
+								<div class="flex flex-col space-y-4 pt-4 sm:flex-row sm:space-y-0 sm:space-x-4">
+									{#each months as month (month.value)}
+										<DatePicker.Grid class="w-full border-collapse space-y-1 select-none">
+											<DatePicker.GridHead>
+												<DatePicker.GridRow class="mb-1 flex w-full justify-between">
+													{#each weekdays as day, i (i)}
+														<DatePicker.HeadCell
+															class="text-muted-foreground w-10 rounded-md text-xs font-normal!"
+														>
+															<div>{day.slice(0, 2)}</div>
+														</DatePicker.HeadCell>
+													{/each}
+												</DatePicker.GridRow>
+											</DatePicker.GridHead>
+											<DatePicker.GridBody>
+												{#each month.weeks as weekDates (weekDates)}
+													<DatePicker.GridRow class="flex w-full">
+														{#each weekDates as date (date)}
+															<DatePicker.Cell
+																{date}
+																month={month.value}
+																class="relative size-13 p-0! text-center"
+															>
+																<DatePicker.Day
+																	class="data-selected:text-background group relative inline-flex size-13 cursor-pointer items-center justify-center rounded-lg border border-transparent p-0 text-base font-light font-normal whitespace-nowrap transition-all hover:bg-amber-200 data-disabled:pointer-events-none data-outside-month:pointer-events-none data-outside-month:text-black/40 data-selected:bg-amber-400 data-selected:font-medium data-unavailable:line-through data-unavailable:opacity-50"
+																>
+																	<div
+																		class="absolute top-[5px] size-2 rounded-full bg-black opacity-40 transition-all group-data-selected:bg-amber-200 group-data-today:block"
+																	></div>
+																	{date.day}
+																</DatePicker.Day>
+															</DatePicker.Cell>
+														{/each}
+													</DatePicker.GridRow>
+												{/each}
+											</DatePicker.GridBody>
+										</DatePicker.Grid>
+									{/each}
+								</div>
+							{/snippet}
+						</DatePicker.Calendar>
+					</DatePicker.Content>
+				</DatePicker.Root>
 			</div>
 		</div>
 		<button onclick={dateNext} class="arrowButton group">
