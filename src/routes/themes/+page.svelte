23a4d72ac38660ff203b6d03980994745deb9e77
Ryanssd22 <47930762+Ryanssd22@users.noreply.github.com>
2025-07-03 14:53:49 -0500

    Update +page.svelte

--- src/routes/themes/+page.svelte
+++ src/routes/themes/+page.svelte
@@ -1,134 +1,150 @@
 <!-- make it so that the user can either input in a raw hex code, rgb, or the name of a color even to seed the scheme generator-->
 
 <script>
-import { onMount } from "svelte";
+	import { onMount } from 'svelte';
+	import { fade } from 'svelte/transition';
 
-let liturgicalThemes = [];
+	let liturgicalThemes = [];
 
-let whiteTheme = {
-    primary: "#FFFEF2",
-    secondary: "#CBB06A",
-    tertiary: "#D8D8D6",
-    title: "White",
-    class: "theme-white"
-}
+	let whiteTheme = {
+		primary: '#FFFEF2',
+		secondary: '#CBB06A',
+		tertiary: '#D8D8D6',
+		title: 'White',
+		class: 'theme-white'
+	};
 
-let greenTheme = {
-    primary: "#336958",
-    secondary: "#CBB06A",
-    tertiary: "#7c9993",
-    title: "Green",
-    class: "theme-green"
-}
+	let greenTheme = {
+		primary: '#336958',
+		secondary: '#CBB06A',
+		tertiary: '#7c9993',
+		title: 'Green',
+		class: 'theme-green'
+	};
 
-let purpleTheme = {
-    primary: "#2F195F",
-    secondary: "#CBB06A",
-    tertiary: "#7353BA",
-    title: "Purple",
-    class: "theme-purple"
-}
+	let purpleTheme = {
+		primary: '#2F195F',
+		secondary: '#CBB06A',
+		tertiary: '#7353BA',
+		title: 'Purple',
+		class: 'theme-purple'
+	};
 
-let redTheme = {
-    primary: "#9a122d",
-    secondary: "#CBB06A",
-    tertiary: "#d9686d",
-    title: "Red",
-    class: "theme-red"
-}
+	let redTheme = {
+		primary: '#9a122d',
+		secondary: '#CBB06A',
+		tertiary: '#d9686d',
+		title: 'Red',
+		class: 'theme-red'
+	};
 
-let roseTheme = {
-    primary: "#b35cb5",
-    secondary: "#CBB06A",
-    tertiary: "#f6bcf7",
-    title: "Rose",
-    class: "theme-rose"
-}
+	let roseTheme = {
+		primary: '#b35cb5',
+		secondary: '#CBB06A',
+		tertiary: '#f6bcf7',
+		title: 'Rose',
+		class: 'theme-rose'
+	};
 
-liturgicalThemes.push(whiteTheme, greenTheme, purpleTheme, redTheme, roseTheme);
+	liturgicalThemes.push(whiteTheme, greenTheme, purpleTheme, redTheme, roseTheme);
 
-let currentTheme = liturgicalThemes[4];
-let currentName = currentTheme.class;
+	let currentTheme = $state(liturgicalThemes[4]);
+	let currentName = $state(currentTheme.class);
 
-function chooseTheme(theme) {
-    console.log("Selected theme: ", theme);
-    currentTheme = theme;
-    currentName = theme.class;
-}
+	function chooseTheme(i) {
+		let theme = liturgicalThemes[i];
+		console.log('Selected theme: ', theme);
+		currentTheme = theme;
+		currentName = theme.class;
+		localStorage.setItem('theme', i);
+	}
 
+	let ready = $state(false);
+	onMount(() => {
+		let theme = localStorage.getItem('theme');
+		console.log(theme);
+		// localStorage.setItem('theme', '0');
+		chooseTheme(theme);
+		ready = true;
+	});
 </script>
 
-
-
-<style>
-    .grid {
-	display: grid;
-	grid-template-columns: repeat(5, 100px);
-	gap: 34px;
-    }
-
-    .themeButton {
-	background-color: red;
-	height: 100px;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	cursor: pointer;
-    }
-
-</style>
-
-
-<h1 class="mb-4 text-4xl leading-none tracking-tight text-gray-800 md:text-5xl mt-8">Default</h1>
+<h1 class="mt-8 mb-4 text-4xl leading-none tracking-tight text-gray-800 md:text-5xl">Default</h1>
 
 <div class="section themes fullWidth">
-    <div class="grid">
-	{#each liturgicalThemes as theme, i}
-	    <div class="themeButton group hover:scale-108 active:scale-100 transition-all select-none flex flex-col shadow-lg rounded-full"
-		on:click = {() => {chooseTheme(theme)}}
-		style="
+	<div class="grid">
+		{#each liturgicalThemes as theme, i}
+			<div
+				class="themeButton group flex flex-col rounded-full shadow-lg transition-all select-none hover:scale-108 active:scale-100"
+				on:click={() => {
+					chooseTheme(i);
+				}}
+				style="
 		background-color: {theme.primary};
 		color: {theme.tertiary};
 		border-radius: 0.5rem;
 		"
-	    >
-		{theme.title}
+			>
+				{theme.title}
 
-
-		<div class="flex gap-2 mt-2 opacity-0 group-hover:opacity-100 transition-opacity duration-200">
-		    <div class="w-4 h-4 rounded-full" style="background-color: {theme.secondary}"></div>
-		    <div class="w-4 h-4 rounded-full" style="background-color: {theme.tertiary}"></div>
-		</div>
-
-	    </div>
-	{/each}
-
-    </div>
+				<div
+					class="mt-2 flex gap-2 opacity-0 transition-opacity duration-200 group-hover:opacity-100"
+				>
+					<div class="h-4 w-4 rounded-full" style="background-color: {theme.secondary}"></div>
+					<div class="h-4 w-4 rounded-full" style="background-color: {theme.tertiary}"></div>
+				</div>
+			</div>
+		{/each}
+	</div>
 </div>
 
-<h1 class="mb-4 text-4xl leading-none tracking-tight text-gray-800 md:text-5xl mt-8">Themes</h1>
+<h1 class="mt-8 mb-4 text-4xl leading-none tracking-tight text-gray-800 md:text-5xl">Themes</h1>
 
-<div class="section themes fullWidth {currentName}">
-    <div class="grid">
-	{#each liturgicalThemes as theme, i}
-	    <div class="themeButton group hover:scale-108 active:scale-100 transition-all select-none flex flex-col shadow-lg rounded-full"
-		on:click = {() => {chooseTheme(theme)}}
-		style="
+{#if ready}
+	<div transition:fade class="section themes fullWidth {currentName}">
+		<div class="grid">
+			{#each liturgicalThemes as theme, i}
+				<div
+					class="themeButton group flex flex-col rounded-full shadow-lg transition-all select-none hover:scale-108 active:scale-100"
+					on:click={() => {
+						chooseTheme(theme);
+					}}
+					style="
 		background-color: var(--color-primary);
 		color: var(--color-tertiary);
 		border-radius: 0.5rem;
 		"
-	    >
-		TESTING
+				>
+					TESTING
 
-
-		<div class="flex gap-2 mt-2 opacity-0 group-hover:opacity-100 transition-opacity duration-200">
-		    <div class="w-4 h-4 rounded-full" style="background-color: var(--color-secondary)"></div>
-		    <div class="w-4 h-4 rounded-full" style="background-color: var(--color-tertiary)"></div>
+					<div
+						class="mt-2 flex gap-2 opacity-0 transition-opacity duration-200 group-hover:opacity-100"
+					>
+						<div
+							class="h-4 w-4 rounded-full"
+							style="background-color: var(--color-secondary)"
+						></div>
+						<div class="h-4 w-4 rounded-full" style="background-color: var(--color-tertiary)"></div>
+					</div>
+				</div>
+			{/each}
 		</div>
+	</div>
+{/if}
 
-	    </div>
-	{/each}
+<style>
+	.grid {
+		display: grid;
+		grid-template-columns: repeat(5, 100px);
+		gap: 34px;
+	}
 
-    </div>
-</div>
+	.themeButton {
+		background-color: red;
+		height: 100px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		cursor: pointer;
+	}
+</style>
